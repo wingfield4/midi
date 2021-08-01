af4af76528882eea628f726734c098eed5b6e804
David Wingfield <d.wingfield815@gmail.com>
2021-08-01 00:08:45 -0400

    Add coming soon message

--- src/components/learnChords/LearnChordsComponent.js
+++ src/components/learnChords/LearnChordsComponent.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Text from '../common/mui/Text';
+
 const LearnChordsComponent = () => {
   return (
-    <></>
+    <>
+      <Text variant="h2" component="h1">
+        Coming Soon!
+      </Text>
+    </>
   )
 }
 
